72190a43c329e50b67d446058a5fdf343d0baf99
Fortunatus <njokufortune10@gmail.com>
2026-08-27 17:12:31 +0100

    render yaml

--- Wristhaus_store/settings.py
+++ Wristhaus_store/settings.py
@@ -91,6 +91,14 @@ DATABASES = {
     }
 }
 
+# Override database with DATABASE_URL if it exists
+database_url = os.environ.get('DATABASE_URL')
+if database_url:
+    import dj_database_url
+    DATABASES['default'] = dj_database_url.config(default=database_url, conn_max_age=600)
+
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
