eec2d856dee0b0726b056a32321e73635bdf45f5
Fortunatus <njokufortune10@gmail.com>
2026-08-30 12:17:29 +0100

    commit

--- Wristhaus_store/settings.py
+++ Wristhaus_store/settings.py
@@ -34,11 +34,7 @@ SECRET_KEY = 'django-insecure-ph+9s-00jhs9%543xtk8w2rjf0upl9!q9$1*x+c$xz_)$jhj%p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-
-if DEBUG:
-    ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
-else:
-    ALLOWED_HOSTS = ["wristhaus-website.onrender.com"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
